a7b0be4c0d1963d4a32ac8132d5d9ccdbb1ae325
Bill Lan <bilan604gm@gmail.com>
2023-06-27 02:56:12 -0700

    Add git push script

---
+++ finish.sh
@@ -0,0 +1,7 @@
+git status;
+git add .;
+git commit -m "${1}";
+git push origin ${2};
+echo "done";
+exit;
+
---
+++ start.sh
@@ -0,0 +1,5 @@
+git pull origin;
+git checkout -b development;
+source /main/Scripts/activate;
+echo "done";
+exit;
