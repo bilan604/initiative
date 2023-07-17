cb2a3f27d4dd03633e99c6eadb236bbf3b076392
Bill Lan <bilan604gm@gmail.com>
2023-07-17 14:18:08 -0700

    Update

--- README.md
+++ README.md
@@ -2,19 +2,6 @@
 
 This repository is the remote head for aggregating code into one place. If you are git cloning this, you will have to replace the files with placeholders. Most of the code will be added to the .gitignore, because it may or may not have been written for projects not pertaining to this endpoint itself.  
 
-However, if git cloning, you will just need to replace the handler file with placeholder functions. This repository can be used as a template for your own endpoint.  
-
-## 1. Autofill Chrome Extension:  
-1. Git clone this repository.  
-2. Install the dependencies.  
-3. Open the chome-extension and go to bachground.js. Fill in your GPT-4 API key on line 111.  
-4. run `python main.py` in git bash to start the server.  
-5. Go to Google Chrome -> More Tools -> Extensions.  
-6. Toggle developer mode to on (top right).  
-7. Click load unpacked (top left), and select the folder 'chrome-extension'.  
-8. Toggle the extension to on if it is not on by default.  
-9. Click 'background page' and go to console to see the console logs and verify it is working.  
-
 #### Ping:  
 Check the landing page is up:
 Visit http://10.0.0.179:8000
--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -1,35 +1,36 @@
 import json
 import requests
+from tests.static_info import *
 
+def api_request(userId, operation, request_data):
+    # Specify an endpoint in main
+    endpoint = "http://10.0.0.179:8000/api/"
 
-def test_src():
-    url = "https://docs.google.com/forms/d/e/1FAIpQLScMDHIfobvFjHrOXTwERHskYutiCzgBfA6iAQBkRlijXT92MA/viewform"    
-    src = requests.get(url).text
     req_params = {
-    "userId": "testId",
-    "operation": "get_question_answer_prompts",
-    "requestData": json.dumps({
-            "html_content": src
-            })
-    }        
-    response = requests.get("http://10.0.0.179:8000/api/", params=req_params)
-    print(response.text)
-    return response.text
-
-
-def test_url(api_key):
-
-    url = "https://docs.google.com/forms/d/e/1FAIpQLScMDHIfobvFjHrOXTwERHskYutiCzgBfA6iAQBkRlijXT92MA/viewform"
-    
-    req_params = {
-    "userId": "testId",
-    "operation": "get_questions_from_url_with_llm",
-    "requestData": json.dumps({
-            "api_key": api_key,
-            "url": url
-            })
+        "id": userId,
+        "operation": operation,
+        "request_data": json.dumps(request_data)
     }
-    response = requests.get("http://10.0.0.179:8000/api/", params=req_params)
-    print(response.text)
-    print("test2: len(response.text)", len(response.text))
+
+    response = requests.get(endpoint, params=req_params)
+    print("\nresponse:", response)
     
+    return json.loads(response.text)
+
+request_data = {
+    "tablename": "input",
+    "query": "How many years of Python programming experience do you have?"
+}
+
+response = api_request("bilan604", "search_datatable", request_data)
+easy_apply_src = get_easy_apply_src()
+
+request_data = {
+    "src": easy_apply_src,
+    "rule": 'lambda x: "class" in x and "jobs-easy-apply-form-section__grouping" in x["class"]'
+}
+
+response = api_request("bilan604", "get_extracted_questions", request_data)
+
+for r in response:
+    print("Question:", r["question"])
