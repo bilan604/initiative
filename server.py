31945bd75a1a619778644b36d016327aca569088
Bill Lan <bilan604gm@gmail.com>
2023-07-18 07:41:41 -0700

    Update

--- server.py
+++ server.py
@@ -1,21 +1,16 @@
 import os
 import json
-# Note: request has been redeclared
 from flask import Flask, request, redirect, render_template, url_for
 from src.handling import get_search_result_links
 from src.handling import search_datatable
 from src.handling import get_extracted_questions
-
-
 from dotenv import load_dotenv
 load_dotenv()
 
 # count is unused
 count = 0
 
-
 app = Flask(__name__)
-
 
 operationFunctionsMap = {
     # Utility function
@@ -24,17 +19,6 @@ operationFunctionsMap = {
     "get_extracted_questions": get_extracted_questions
 }
 
-"""
-# Wrapper function: getUrlQuestionsWithLLM
-"get_question_answer_prompts": get_question_answer_prompts,
-# Wrapper function: getUrlQuestionsWithLLM
-"get_question_answer_prompt_responses": get_question_answer_prompt_responses,
-# Requires API Key
-"get_questions_from_url_with_llm": get_questions_from_url_with_llm,
-
-"question_answer_fast": do_question_answer_fast, 
-"answer_input_questions": do_answer_input_questions,
-"""
 
 def operationFunctionHandler(requestorId, operation, requestData):
     print("requestorId:", requestorId)
@@ -44,18 +28,21 @@ def operationFunctionHandler(requestorId, operation, requestData):
 
 @app.route("/api/", methods=("GET", "POST"))
 def api():
-    print("Function call, api()")
+    print("==> api() function called")
     userId = request.args.get("id")
     operation = request.args.get("operation")
     request_data = json.loads(request.args.get("request_data"))
+
     if request.method == "GET":
         print("GET", userId, operation)
         api_response = operationFunctionHandler(userId, operation, request_data)
         return json.dumps(api_response)
+    
     elif request.method == "POST":
         print("POST", userId, operation)
         api_response = operationFunctionHandler(userId, operation, request_data)
         return json.dumps(api_response)
+    
     return "None"
 
 
@@ -65,11 +52,9 @@ def index():
         return "TBH"
     return render_template("index.html")
 
+
 def run_app():
     path = "/".join(os.getcwd().split("\\"))
     os.chdir(path)
     app.run(host="10.0.0.179", port=8000)
-
-
-    
 
--- src/handling.py
+++ src/handling.py
@@ -1,5 +1,5 @@
+import re
 import json
-import requests
 from src.operator.search.searching import get_search_result_links
 from src.operator.search.searching import get_datatable, query_datatable
 from src.operator.question.extract import get_questions
@@ -7,6 +7,8 @@ from src.operator.question.extract import get_questions
 """
 This file exists as a wrapper for the functionalities that are accessed,
 because inputs have to be verified and stuff, etc
+
+Perhaps it could serve as proxy for a the information in a system design interview?
 """
 
 
@@ -24,9 +26,7 @@ def load_datatable(id, data):
     """
     Loads to RAM, is placeholder, replaceable component
     """
-    print("load_data()")
-    print("data:", data)
-    print("\ntype(data):", type(data))
+    print("load_datatable()")
 
     global DATATABLES
     # add validations, etc
@@ -36,9 +36,7 @@ def load_datatable(id, data):
 
 
 def search_datatable(id, data):
-    print("load_data()")
-    print("data:", data)
-    print("\ntype(data):", type(data))
+    print("search_datatable()")
 
     # validations, etc
     tablename = data["tablename"]
@@ -59,37 +57,36 @@ def search_datatable(id, data):
 
 
 def get_extracted_questions(id, data):
-    print("load_data()")
-    print("data:", data)
-    print("\ntype(data):", type(data))
-
-    # validations, etc
-    # validations, etc
-    # validations, etc!
+    print("get_extracted_questions()")
+    # Add validations!
 
     src = data["src"]
     if not src:
         return "not src"
     
     rule_str = data["rule"]
-    
+    original_str = rule_str
+
     global RULE
     RULE = None
     if not rule_str:
         return "not rule_str"
     else:
+        rule_str = re.sub("\\", "", rule_str)
+        rule_str = re.sub("\n", "", rule_str)
+        rule_str = re.sub("#", "", rule_str)
+        rule_str = re.sub("global", "", rule_str)
+        if original_str != rule_str:
+            return "Bad!"
+
         script = "global RULE\n"
         script += "RULE = " + rule_str + "\n"
-        print("\nscript:", script)        
         try:
             exec(script)
         except:
             return "Error on exec(script)"
-    
-    print("\nrule:", RULE)
 
     questions = get_questions(src, RULE)
-    
     print("\nquestions:", questions)
 
     return questions
