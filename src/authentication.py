a690ad856befb951dfdf2d015ba382a0982328a7
Bill Lan <bilan604gm@gmail.com>
2023-09-05 19:45:01 -0700

    Update files

--- src/authentication.py
+++
@@ -1,1 +0,0 @@
-# This is a placeholder until a db is set up
--- src/handler/data_structures/classic.py
+++
@@ -1,6 +0,0 @@
-class TrieNode(object):
-    def __init__(self, val, content="", children={}):
-        self.val = val
-        self.content = content
-        self.children = children
-
